8553e8c28ee8555073a5190045632b3dc5976ada
Sofiya112 <sofiya032005@gmail.com>
2025-05-22 11:04:36 +0300

    Add files via upload

---
+++ Task 3 pr 43/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_pr_43
+{
+    struct Book
+    {
+        public string title;        // Название книги
+        public string author;       // Ф.И.О. автора
+        public int publicationYear; // Год издания
+        public int pageCount;       // Количество страниц
+
+        // Конструктор для инициализации полей
+        public Book(string title, string author, int publicationYear, int pageCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.publicationYear = publicationYear;
+            this.pageCount = pageCount;
+        }
+
+        // Метод для вывода информации о книге
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Название: {title}");
+            Console.WriteLine($"Автор: {author}");
+            Console.WriteLine($"Год издания: {publicationYear}");
+            Console.WriteLine($"Количество страниц: {pageCount}");
+            Console.WriteLine(); // Для разделения книг
+        }
+    }
+}
---
+++ Task 3 pr 43/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_pr_43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Ввод количества книг
+            Console.Write("Введите количество книг: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Создание массива книг
+            Book[] books = new Book[n];
+
+            // Ввод информации о каждой книге
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nВвод информации для книги #{i + 1}:");
+                Console.Write("Введите название книги: ");
+                string title = Console.ReadLine();
+
+                Console.Write("Введите Ф.И.О. автора: ");
+                string author = Console.ReadLine();
+
+                Console.Write("Введите год издания: ");
+                int publicationYear = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите количество страниц: ");
+                int pageCount = int.Parse(Console.ReadLine());
+
+                // Создание экземпляра книги и добавление в массив
+                books[i] = new Book(title, author, publicationYear, pageCount);
+            }
+
+            // Вывод информации о всех книгах
+            Console.WriteLine("\nИнформация о книгах:");
+            for (int i = 0; i < n; i++)
+            {
+                books[i].DisplayInfo();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
