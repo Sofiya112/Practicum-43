8c17c059607fd4b7f9fc6c7a7fe1c357973339e9
Kab-35-16 <Kab-35-16@KAB-31-3->
2025-05-22 11:00:10 +0300

    Добавьте файлы проекта.

---
+++ MyClass.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_43
+{
+    class MyClass
+    {
+        public string change; //изменять
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_43
+{
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено"; // Меняем значение поля
+        }
+
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено"; // Меняем значение поля
+        }
+
+        static void Main(string[] args)
+        {
+            // Создание экземпляра класса MyClass
+            MyClass myClassInstance = new MyClass();
+            myClassInstance.change = "не изменено"; 
+
+            // Создание экземпляра структуры MyStruct
+            MyStruct myStructInstance = new MyStruct();
+            myStructInstance.change = "не изменено"; 
+
+            
+            Console.WriteLine("До изменений:");
+            Console.WriteLine($"myClassInstance.change: {myClassInstance.change}");
+            Console.WriteLine($"myStructInstance.change: {myStructInstance.change}");
+
+            ClassTaker(myClassInstance); // Изменение поля в классе
+            StruktTaker(myStructInstance); // Изменение поля в структуре
+
+            // Выводим значения после изменений
+            Console.WriteLine("\nПосле изменений:");
+            Console.WriteLine($"myClassInstance.change: {myClassInstance.change}");
+            Console.WriteLine($"myStructInstance.change: {myStructInstance.change}");
+
+            Console.ReadLine();
+        }
+        
+    }
+}
