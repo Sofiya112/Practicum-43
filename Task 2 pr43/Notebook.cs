f8e41e3ea0bd48d5dd63849c87bc3dec50221f10
Sofiya112 <sofiya032005@gmail.com>
2025-05-22 11:03:40 +0300

    Add files via upload
    Задание 2. Используя Visual Studio, создайте проект по шаблону Console Application.
    
    Создайте структуру с именем - Notebook.
    
    Поля структуры: модель, производитель, цена.
    
    В структуре должен быть реализован конструктор для инициализации полей и метод для вывода содержимого полей на экран.

---
+++ Task 2 pr43/Notebook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_pr43
+{
+    struct Notebook
+    {
+        public string model; //Модель
+        public string manufacturer; //Производитель
+        public double price; //Цена
+
+        public Notebook(string model, string manufacturer, double price) //Конструткор
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Модель: {model}");
+            Console.WriteLine($"Производитель: {manufacturer}");
+            Console.WriteLine($"Цена: {price} руб.");
+        }
+    }
+}
---
+++ Task 2 pr43/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_pr43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook myNotebook = new Notebook("ThinkPad X1", "Lenovo", 85000);
+
+            // Вывод информации о ноутбуке
+            myNotebook.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
